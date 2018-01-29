8b23a5f2841ffc291cc167e99bb3c3803641536f
Thanner Soares <thannersoares@gmail.com>
2018-01-29 13:57:52 -0200

    Changes in processmodelbuilder

--- src/main/java/br/edu/ufrgs/inf/bpm/wrapper/ProcessModelBuilder.java
+++ src/main/java/br/edu/ufrgs/inf/bpm/wrapper/ProcessModelBuilder.java
@@ -1,28 +1,212 @@
-package br.edu.ufrgs.inf.bpm.wrapper;/*
+/*
 package br.edu.ufrgs.inf.bpm.wrapper;
 
-import processToText.dataModel.process.ProcessModel;
+import processToText.dataModel.process.*;
+
+import java.io.InvalidObjectException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProcessModelBuilder {
 
-    private ProcessModel processModel;
-    private ProcessModelWrapper processModelWrapper;
+    private int genericId;
+    private Map<String, Lane> laneMap;
+    private Map<String, Pool> poolMap;
+    private Map<String, Element> elementMap;
+    private Map<String, Integer> idMap;
+    private BpmnXmlWrapper bpmnXmlWrapper;
 
     public ProcessModelBuilder() {
-        this.processModelWrapper = new ProcessModelWrapper();
+        genericId = 0;
+        laneMap = new HashMap<>();
+        poolMap = new HashMap<>();
+        elementMap = new HashMap<>();
+        idMap = new HashMap<>();
     }
 
-    public ProcessModel buildProcess(br.edu.ufrgs.inf.bpm.process.Process process) {
-        processModel = new ProcessModel(process.getId(), process.getName());
+    public ProcessModel buildProcess(String bpmnString) {
+        int newId = generateModelId("ProcessModel1");
+        ProcessModel processModel = new ProcessModel(newId, "Process Model");
 
-        process.getPools().forEach(p -> processModelWrapper.addPool(processModel, p));
-        process.getLanes().forEach(l -> processModelWrapper.addLane(processModel, l));
-        process.getActivites().values().forEach(a -> processModelWrapper.addActivity(processModel, a));
-        process.getEvents().values().forEach(e -> processModelWrapper.addEvent(processModel, e));
-        process.getGateways().values().forEach(g -> processModelWrapper.addGateway(processModel, g));
-        process.getArcs().values().forEach(a -> processModelWrapper.addArc(processModel, a));
+        try {
+            bpmnXmlWrapper = new BpmnXmlWrapper(bpmnString);
+            bpmnXmlWrapper.getModelElementsByType(LaneSet.class).forEach(p -> processModel.addPool(createPool(p)));
+            bpmnXmlWrapper.getModelElementsByType(TLane.class).forEach(p -> processModel.addLane(createLane(p)));
+            bpmnXmlWrapper.getModelElementsByType(TActivity.class).forEach(p -> processModel.addActivity(createActivity(p)));
+            bpmnXmlWrapper.getModelElementsByType(TEvent.class).forEach(p -> processModel.addEvent(createEvent(p)));
+            bpmnXmlWrapper.getModelElementsByType(TGateway.class).forEach(p -> processModel.addGateway(createGateway(p)));
+            bpmnXmlWrapper.getModelElementsByType(SequenceFlow.class).forEach(p -> processModel.addArc(createArc(p)));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
         return processModel;
     }
-}
+
+    private String createPool(TLaneSet pool) {
+        int newId = generateModelId(pool.getId());
+        Pool modelPool = new Pool(newId, getName(pool.getName()));
+        poolMap.put(pool.getId(), modelPool);
+        return modelPool.getName();
+    }
+
+    private String createLane(TLane lane) {
+        int newId = generateModelId(lane.getId());
+        Lane modelLane = new Lane(newId, getName(lane.getName()), getPoolByLane(lane));
+        laneMap.put(lane.getId(), modelLane);
+        return modelLane.getName();
+    }
+
+    private Activity createActivity(TActivity activity) {
+        try {
+            int newId = generateModelId(activity.getId());
+            int activityType = getActivityType(activity.getElementType().getTypeName());
+            Activity modelActivity = new Activity(newId, getName(activity.getName()), getLaneByObject(activity), getPoolByObject(activity), activityType);
+            elementMap.put(activity.getId(), modelActivity);
+            return modelActivity;
+        } catch (InvalidObjectException i) {
+            i.printStackTrace();
+        }
+        return null;
+    }
+
+    private Event createEvent(TEvent event) {
+        try {
+            int newId = generateModelId(event.getId());
+            int eventType = getEventType(event.getElementType().getTypeName());
+            Event modelEvent = new Event(newId, getName(event.getName()), getLaneByObject(event), getPoolByObject(event), eventType);
+            elementMap.put(event.getId(), modelEvent);
+            return modelEvent;
+        } catch (InvalidObjectException i) {
+            i.printStackTrace();
+        }
+        return null;
+    }
+
+    private Gateway createGateway(TGateway gateway) {
+        try {
+            int newId = generateModelId(gateway.getId());
+            int gatewayType = getGatewayType(gateway.getElementType().getTypeName());
+            Gateway modelGateway = new Gateway(newId, getName(gateway.getName()), getLaneByObject(gateway), getPoolByObject(gateway), gatewayType);
+            elementMap.put(gateway.getId(), modelGateway);
+            return modelGateway;
+        } catch (InvalidObjectException i) {
+            i.printStackTrace();
+        }
+        return null;
+    }
+
+    private Arc createArc(TSequenceFlow arc) {
+        int newId = generateModelId(arc.getId());
+        return new Arc(newId, getName(arc.getName()), elementMap.get(arc.getSource().getId()), elementMap.get(arc.getTarget().getId()));
+    }
+
+    private String getName(String name) {
+        return name != null ? name : "";
+    }
+
+    private int getActivityType(String typeName) throws InvalidObjectException {
+        switch (typeName) {
+            case "task":
+                return 0;
+            case "subProcess":
+                return 3;
+            default:
+                throw new InvalidObjectException(typeName);
+        }
+    }
+
+    private int getEventType(String typeName) throws InvalidObjectException {
+        switch (typeName) {
+            case "startEvent":
+                return 1;
+            case "endEvent":
+                return 11;
+            default:
+                throw new InvalidObjectException(typeName);
+        }
+
+        // public static final int START_EVENT = 1;
+        // public static final int START_MSG = 2;
+        // public static final int START_TIMER = 3;
+
+        // public static final int END_EVENT = 11;
+        // public static final int END_ERROR = 12;
+        // public static final int END_SIGNAL = 13;
+        // public static final int END_MSG = 14;
+
+        // public static final int INTM = 21;
+        // public static final int INTM_TIMER = 22;
+        // public static final int INTM_CANCEL = 23;
+        // public static final int INTM_CONDITIONAL = 24;
+        // public static final int INTM_ESCALATION = 25;
+        // public static final int INTM_ERROR = 26;
+
+        // public static final int INTM_ESCALATION_THR = 31;
+        // public static final int INTM_SIGNAL_THR = 32;
+        // public static final int INTM_MULTIPLE_THR = 33;
+        // public static final int INTM_LINK_THR = 34;
+        // public static final int INTM_MSG_THR = 35;
+
+        // public static final int INTM_ESCALATION_CAT = 41;
+        // public static final int INTM_MULTIPLE_CAT = 42;
+        // public static final int INTM_LINK_CAT = 43;
+        // public static final int INTM_MSG_CAT = 44;
+        // public static final int INTM_PMULT_CAT = 45;
+        // public static final int INTM_COMPENSATION_CAT = 46;
+    }
+
+    private int getGatewayType(String typeName) throws InvalidObjectException {
+        switch (typeName) {
+            case "exclusiveGateway":
+                return 0;
+            case "inclusiveGateway":
+                return 1;
+            case "parallelGateway":
+                return 2;
+            case "eventBasedGateway":
+                return 3;
+            default:
+                throw new InvalidObjectException(typeName);
+        }
+    }
+
+    private Pool getPoolByLane(TLane lane) {
+        ModelElementInstance parent = lane.getParentElement();
+        if (parent.getElementType().getTypeName().equalsIgnoreCase(LaneSet.class.getSimpleName())) {
+            LaneSet laneSet = (LaneSet) parent;
+            return poolMap.get(laneSet.getId());
+        } else {
+            return null;
+        }
+    }
+
+    private Pool getPoolByObject(FlowNode flowNode) {
+        LaneSet pool = bpmnXmlWrapper.getPoolByFlowNode(flowNode);
+        if (pool != null) {
+            return poolMap.get(pool.getId());
+        } else {
+            return null;
+        }
+    }
+
+    private Lane getLaneByObject(FlowNode flowNode) {
+        TLane lane = bpmnXmlWrapper.getLaneByFlowNode(flowNode);
+        if (lane != null) {
+            return laneMap.get(lane.getId());
+        } else {
+            return null;
+        }
+    }
+
+    private int generateModelId(String oldId) {
+        int newId = genericId++;
+        idMap.put(oldId, newId);
+        return newId;
+    }
+
+    public Map<String, Integer> getIdMap() {
+        return idMap;
+    }
 */
