41fdd0975043e2f5e248908718defde43ceedddd
Thanner Soares <thannersoares@gmail.com>
2018-01-31 11:44:50 -0200

    Update Paths

--- src/main/java/br/edu/ufrgs/inf/bpm/util/Path.java
+++ src/main/java/br/edu/ufrgs/inf/bpm/util/Path.java
@@ -6,5 +6,7 @@ public class Path {
     public static String WordNetPath = "src/main/resources/WordNet/WordNet_properties.xml";
     public static String StanfordBidirectionalDistsimPath = "/StanfordParser/tagger/wsj-0-18-bidirectional-distsim.tagger";
 
+    public static final String SentenceTemplatePath = "/SentenceTemplates/";
+
     public static String packageBpmnPath = "br.edu.ufrgs.inf.bpm.bpmn";
 }
