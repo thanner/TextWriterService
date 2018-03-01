439290976eb3dc4d54192def152aeb032c680d16
Thanner Soares <thannersoares@gmail.com>
2018-03-01 13:33:20 -0300

    Get process element components (without comments)

--- src/main/java/br/edu/ufrgs/inf/bpm/changes/sentenceRealization/SurfaceRealizer.java
+++ src/main/java/br/edu/ufrgs/inf/bpm/changes/sentenceRealization/SurfaceRealizer.java
@@ -124,57 +124,6 @@ public class SurfaceRealizer {
         return indexStart + subsentence.length();
     }
 
-    /*
-    public String realizeSentenceMap(ArrayList<DSynTSentence> sentencePlan, HashMap<Integer, String> map) {
-        String s = "<text>\n";
-        for (DSynTSentence dsynt : sentencePlan) {
-            s = s + " " + realizeMapSentence(dsynt, map) + "\n";
-        }
-        return s + "</text>";
-    }
-
-    private String realizeMapSentence(DSynTSentence s, HashMap<Integer, String> map) {
-        Document xmldoc = s.getDSynT();
-        realproManager.realize(xmldoc);
-        ArrayList<Integer> ids = s.getExecutableFragment().getAssociatedActivities();
-        if (s.getClass().toString().endsWith("DSynTConditionSentence")) {
-            DSynTConditionSentence cs = (DSynTConditionSentence) s;
-            ids.addAll(cs.getConditionFragment().getAssociatedActivities());
-            ArrayList<ConditionFragment> sentences = cs.getConditionFragment().getSentenceList();
-            if (sentences != null) {
-                for (ConditionFragment cFrag : sentences) {
-                    ids.addAll(cFrag.getAssociatedActivities());
-                }
-            }
-        } else {
-            DSynTMainSentence ms = (DSynTMainSentence) s;
-            ArrayList<ExecutableFragment> sentences = ms.getExecutableFragment().getSentencList();
-            if (sentences != null) {
-                for (ExecutableFragment eFrag : sentences) {
-                    ids.addAll(eFrag.getAssociatedActivities());
-                }
-            }
-        }
-        String output = "";
-        c++;
-        String idAttr = "";
-        for (int i = 0; i < ids.size(); i++) {
-            if (i > 0) {
-                idAttr = idAttr + ",";
-            }
-            idAttr = idAttr + map.get(ids.get(i));
-        }
-
-        return output + "<phrase ids=\"" + idAttr + "\"> " + realproManager.getSentenceString() + " </phrase>";
-    }
-
-    private String realizeFragment(ConditionFragment cFrag) {
-        Document xmldoc = new DSynTConditionSentence(new ExecutableFragment("", "", "", ""), cFrag).getDSynT();
-        realproManager.realize(xmldoc);
-        return realproManager.getSentenceString();
-    }
-    */
-
     public String postProcessText(String surfaceText) {
         surfaceText = surfaceText.replaceAll("If it is necessary", "If it is necessary,");
         surfaceText = surfaceText.replaceAll("one of the branches was executed", "one of the branches was executed,");
